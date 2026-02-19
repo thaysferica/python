205bf582f263fd1ff42e38e05667f4cb205f541e
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-02-19 08:17:27 -0300

    Update exemplo.py
    ww

--- exemplo.py
+++ exemplo.py
@@ -2,3 +2,7 @@ print("hello word")
 import this
 print(7+4)
 print("7"+"4")
+nome=input ("Qual é seu nome?")
+idade=input ("qual é sua idade?")
+peso=input("qual é seu peso?")
+print(nome, idade, peso)
