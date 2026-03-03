f6431162e47e223e73f862ed0d63712a029a10c6
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-03-03 09:50:25 -0300

    t
    yhh

--- exercicios/ex04.py
+++ exercicios/ex04.py
@@ -3,3 +3,7 @@
 #float é igual números decimais
 # bool é pra armazenar valores de verdadeiro/falso
 # str armazenas textos/string 
+
+primeiro= int (input('fale um numero'))
+segundo= int (input('fale mais um numero'))
+print(primeiro+segundo) 
---
+++ exercicios/ex05.py
@@ -0,0 +1,4 @@
+nu1 = int (input('digite seu primeiro numero'))
+nu2 = int (input('digite um numero outra vez'))
+soma = nu1+nu2
+print ("a soma entre {} e {} é outra {}" .format (nu1, nu2, soma))
