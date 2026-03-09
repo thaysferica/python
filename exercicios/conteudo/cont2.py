2cab851b7397ba236380e8cb01dc164413ce1c86
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-03-09 11:47:37 -0300

    Update cont2.py
    nn

--- exercicios/conteudo/cont2.py
+++ exercicios/conteudo/cont2.py
@@ -19,4 +19,13 @@ print (5==(2.5*2))
 # 1 expressão dentro do parenteses 
 # 2 potenciação
 # 3 multiplicação e divisão
-# 4soma e subtração
+# 4 soma e subtração
+
+print (5+3*2)
+print (3*5+4**2)
+print (3*(5+4)**2)
+
+# pow (x,y) para fazer potenciação
+print (pow (3,4))
+print (9**(1/2))
+print ("="*5)
