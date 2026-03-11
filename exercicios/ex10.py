95ab5bca67664ea0d610ba2e6b5a78a761fa3b71
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-03-11 10:55:47 -0300

    5
    567uy5

---
+++ exercicios/ex10.py
@@ -0,0 +1,4 @@
+m = float (input('digite um valor em (metro):'))
+c = m*100
+mili = m*1000
+print('o valor {} centimetros é {} e em milimetros é {}'.format(m,c,mili))
---
+++ exercicios/ex11.py
@@ -0,0 +1,3 @@
+n = int (input('digite um numero inteiro'))
+tabu = n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*8, n*9, n*10
+print ('a tabuada de {} é {}'.format(n,tabu))
