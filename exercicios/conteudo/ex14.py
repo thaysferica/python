dfbc709cc9cd87ac03a95a9543fc7a109c7d81fc
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-03-17 12:26:50 -0300

    x
    x

---
+++ exercicios/conteudo/ex14.py
@@ -0,0 +1,4 @@
+prod = float (input("qual é o preço do produto?"))
+preco = prod * 0.05
+desco = prod - preco
+print ('esta com 5% de desconto, antes era {} e com desconto agr {}:'.format(prod,desco))
---
+++ exercicios/conteudo/ex15.py
@@ -0,0 +1,4 @@
+sal = float (input('qual é o salario?'))
+aumento = sal * 0.15
+soma = sal + aumento
+print ('esta com 15% de aumento, antes era {}e com o aumento agr é {}'.format(sal,soma))
