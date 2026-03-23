78250005fe9744c1a5932991cf3a9eb7807111a5
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-03-23 12:22:19 -0300

    ss
    ss

--- exercicios/conteudo/cont4.py
+++ exercicios/conteudo/cont4.py
@@ -1,4 +1,10 @@
-import math
-num = int (input('digite um numero'))
-raiz = math.sqrt(num)
-print('a raiz de {} é igual a {}'.format(num,raiz))
+#import math 
+#num = int (input('digite um numero')) 
+#raiz = math.sqrt(num) 
+#print('a raiz de {} é igual a {}'.format(num,math.ceil(raiz))) 
+#print('a raiz de {} é igual a {}'.format(num,math.floor(raiz)))
+
+#importando funcionalidades especificas
+from math import sqrt, floor
+num = 5 int(input('digite um numero'))
+raiz = sqrt(num)  
