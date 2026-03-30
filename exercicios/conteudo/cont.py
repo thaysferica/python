c5fe3fd170935860e505f9b6c2687ebba385f99e
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-03-30 08:11:21 -0300

    6yy

--- exercicios/conteudo/cont.py
+++ exercicios/conteudo/cont.py
@@ -1,1 +1,5 @@
 import random
+num = random.random()
+num2 = random.randint(1,10) 
+print (num)
+print (num2)
---
+++ exercicios/conteudo/ex18.py
@@ -0,0 +1,3 @@
+import math 
+num = float (input('digite um valor'))
+print (f'o valor digitado foi {num} e a sua porção inteira é {math.trunc(num)}')
