11b67ccb0df0011b96a43402ab9b32d6d32abf71
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-04-06 08:59:59 -0300

    vv

---
+++ exercicios/cont7.py
@@ -0,0 +1,14 @@
+frase = ('curso de análise e desenvolvimento de sistemas')
+print(frase[3]) #isso mostra o espaço da memória
+print(frase[3:46]) #intervalo de caracteres menos o último
+print(frase[3:45:2]) #pula de dois em dois
+print(frase[:7]) #pega do inicio ate o caracter que vc escolheu
+print(frase[7:]) #pega do caracter que  vc escolheu ate o final
+print(frase[41::5]) #pega do caracter que vc escolheu pulando de 3 em 3
+print (len(frase)) #esse metodo conta a quantidade de caracter
+print(frase.count('t')) #procura um caracter especifico na variável
+print(frase.count('t', 0,7)) #procura um caracter especifico dentro de um ccomentario especifico
+print(frase.find('análise')) #find mostra a partir de qual indice começa a pesquisa
+print(frase.find('android')) #o resultado -1 indica que não tem essa sequencia na variável
+print ('curso'in frase) #o in verifica se aquele conjunto de string esta na variável
+
