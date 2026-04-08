a40c66d8ae4d3a7010fbda5236ca7fe2d89953bd
AnaSanches7 <thaynara.couto.pereira@escola.pr.gov.br>
2026-04-08 11:49:44 -0300

    rdr

--- exercicios/cont7.py
+++ exercicios/cont7.py
@@ -11,4 +11,13 @@ print(frase.count('t', 0,7)) #procura um caracter especifico dentro de um ccomen
 print(frase.find('análise')) #find mostra a partir de qual indice começa a pesquisa
 print(frase.find('android')) #o resultado -1 indica que não tem essa sequencia na variável
 print ('curso'in frase) #o in verifica se aquele conjunto de string esta na variável
-
+print (frase.replace('curso', 'instituição')) #troca a palavra
+print (frase.upper()) #deixa em maiusculo
+print (frase.lower()) #deixa em minuscculo
+print (frase.capitalize()) #deixa a primeira letra da primeira palavra em maiusculo
+print (frase.title()) #deixa todas as primeiras letras em maiusculo
+print (frase.strip()) #tira o espaço em branco do inicio e do final
+print (frase.rstrip()) #tira o espaço em branco da direita
+print (frase.lstrip()) #tira o espaço em branco da esquerda
+print (frase.split()) #vai transformar o valor da variavel em uma lista
+print ('%'.join(frase)) #uni o simbolo em cada parte do split
