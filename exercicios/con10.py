ea720c7a35ef2bd2088b9fb3d91fc4d92b26dbff
thaynara <thaynara.couto.pereira@escola.pr.gov.br>
2026-05-25 10:58:15 -0300

    ukjg

--- exercicios/con10.py
+++ exercicios/con10.py
@@ -10,3 +10,20 @@ print ("fim")
 for c in range (0,6,2): 
     print (c)
 print ("fim")
+
+#n = int(input("Digite um número: "))
+#for c in range(0, n + 1 ):#+1 para chegar o número que eu quero
+ #print(c)
+#print("FIM")
+#i = int(input("Inicio: "))
+#f = int(input("Fim: "))
+#p = int(input("Passo: "))
+#for c in range(i, f + 1 , p): #+1 para chegar o número que eu quero
+ #print(c)
+#print("FIM")
+
+s = 0
+for c in range (0,4):
+    n = int (input("digite um valor"))
+    s = s + n
+print (f'o somatório de todos os valores foi {s}')    
---
+++ exercicios/ex48.py
@@ -0,0 +1,6 @@
+import time
+for cont in range(10, -1, -1):
+    print(cont)
+    time.sleep(1) 
+
+print("BUM! BUM! POOW! ")
