6e6a11b9a7273efa42bf808339519507da5bafc5
thaynara <thaynara.couto.pereira@escola.pr.gov.br>
2026-06-30 08:58:32 -0300

    yrry

--- exercicios/ex61.py
+++ exercicios/ex61.py
@@ -1,38 +1,34 @@
 from time import sleep
-
-n1 = int(input('Qual é o primeiro número? '))
-n2 = int(input('Qual é o segundo número? '))
+n1= int(input("Qual é o primeiro número?"))
+n2 = int(input("Qual é o segundo número?"))
 opcao = 0
-
 while opcao != 5:
-    print('''    [ 1 ] somar
-    [ 2 ] multiplicar
-    [ 3 ] maior
-    [ 4 ] novos números
-    [ 5 ] sair do programa''')
-    opcao = int(input('>>>>> Qual é a sua opção? '))
-
-    if opcao == 1:
-        soma = n1 + n2
-        print(f'A soma entre {n1} + {n2} é {soma}')
-    elif opcao == 2:
-        produto = n1 * n2
-        print(f'O resultado de {n1} x {n2} é {produto}')
-    elif opcao == 3:
-        if n1 > n2:
-            maior = n1
-        else:
-            maior = n2
-        print(f'Entre {n1} e {n2} o maior valor é {maior}')
-    elif opcao == 4:
-        print('Informe os números novamente:')
-        n1 = int(input('Primeiro valor: '))
-        n2 = int(input('Segundo valor: '))
-    elif opcao == 5:
-        print('Finalizando...')
+  print('[1] somar')
+  print('[2] multiplicar')
+  print('[3] maior')
+  print('[4] novos números')
+  print('[5] sair do programa')
+  opcao = int(input('Qual é a sua opcão?'))
+  if opcao == 1:
+   soma = n1 + n2
+   print(f'A soma de {n1} + {n2} é igual a {soma}')
+  elif opcao == 2:
+   multiplicacao = n1 * n2
+   print(f'A multiplicação de {n1} e {n2} é igual a {multiplicacao}')
+  elif opcao == 3:
+    if n1 > n2:
+        print(f"0 {n1} é maior que {n2}")
+    elif n2 > n1:
+        print(f"o (n2) é maior que {n1}")
     else:
-        print('Opção inválida. Tente novamente')
-    print('=-=' * 10)
-    sleep(2)
-
-print('Fim do programa! Volte sempre!')
+        print("Ambos os números são iguais")
+  elif opcao == 4:
+    print('informe os números novamente')
+    n1= int(input("Qual é o primeiro número?"))
+    n2= int(input("Qual é o segundo número?"))
+  elif opcao == 5:
+    print('Finalizando...')
+  else:
+      print('opção inválida. Tente novamente')
+sleep(2)
+print('Acabou')
